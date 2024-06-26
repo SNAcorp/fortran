7085cbb67d661f06cd1abd43c87f707e12db2e82
SNA <snagithub@gmail.com>
2024-06-26 17:28:08 +0800

    add docker

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://nikitastepanov@127.0.0.1/fortran"
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
