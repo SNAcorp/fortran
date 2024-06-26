3f4840b819b37a12f5129bfecb7bc8644140e8b0
SNA <snagithub@gmail.com>
2024-06-26 17:40:37 +0800

    mod docker

--- main.py
+++ main.py
@@ -285,4 +285,4 @@ def internal_server_error(request: Request, exc):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
