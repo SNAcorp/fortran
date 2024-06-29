907b56db9db2d55729669cabee7df1dfa1bc40d1
SNA <snagithub@gmail.com>
2024-06-29 11:03:48 +0800

    add optimizes

--- main.py
+++ main.py
@@ -27,15 +27,15 @@ from contextlib import asynccontextmanager
 import jwt
 import redis
 
-# redis_host = os.getenv("REDIS_HOST", "localhost")
-# redis_port = os.getenv("REDIS_PORT", 6379)
-# redis_client = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
-# REDIS_KEY = "public_files"
-# REDIS_TTL = 60  # Время жизни кэша в секундах
-
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+redis_host = os.getenv("REDIS_HOST", "localhost")
+redis_port = os.getenv("REDIS_PORT", 6379)
+redis_client = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
 REDIS_KEY = "public_files"
 REDIS_TTL = 600  # Время жизни кэша в секундах
+
+# redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+# REDIS_KEY = "public_files"
+# REDIS_TTL = 600  # Время жизни кэша в секундах
 
 
 @asynccontextmanager
